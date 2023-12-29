76e42762bba9b4f68b3cc41857ec4e2a2e0a380d
XenonGuardian <shaun.fustin@comcast.net>
2023-12-29 12:58:30 -0500

    Recursion issues with pygame event capture

--- Events.py
+++ Events.py
@@ -9,23 +9,25 @@ pygame.init()
 
 #---Function definitions:---
 def etype():
+        etype = 0
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
                 etype = 99  # Flag that we are done so we exit this loop
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 etype = 1
-        return event.type
+        clock.tick(60)
+        return etype
 
 def uiEvents():
     #-get type of event
-    uiEvents.type = etype
+    uiEvents().cat = etype()
     #-get positions and deltas for x & y
     uiEvents.xpos, uiEvents.ypos = pygame.mouse.get_pos()
     uiEvents.xdelta, uiEvents.ydelta = pygame.mouse.get_pos()
     
 #---Global Variables:---
 pygame.display.set_caption("EventsTracker")
-size = (100, 100)
+size = (500, 500)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 #---Colors---
@@ -38,7 +40,8 @@ screen.fill(WHITE)
 #Loop until the user clicks the close button.
 done = False
 while not done:
-    if uiEvents.type == 99:
+    if uiEvents().cat == 99:
+        print(uiEvents().cat)
         done = True
 
     # Limit to 60 frames per second
